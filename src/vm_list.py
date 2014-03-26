8755dbe17241d3524b7d6ce75ea7b5f155b0f961
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-03-26 22:44:14 +0100

    UID fix, refactorings

--- src/vm_list.py
+++ src/vm_list.py
@@ -1,11 +1,10 @@
-import commands
 import sys
 import os
 from time import gmtime, strftime
 from collections import namedtuple
 from workflow import Workflow
 
-parallels_available = commands.getstatusoutput('which prlctl')[1]
+parallels_available = os.popen('which prlctl').readline().rstrip()
 
 try:
     from vboxapi import VirtualBoxManager
@@ -39,56 +38,49 @@ def complete(wf):
             # Parallels
             if vm_details.state == "stopped":
                 wf.add_item(
-                    'Start', arg='start ' + vm_name,
-                    uid=vm_name, valid=True, icon=vm_details.type, icontype='fileicon')
+                    'Start', arg='start ' + vm_name, valid=True, icon=vm_details.type, icontype='fileicon')
 
             elif vm_details.state == "paused":
                 for action in ['Resume', 'Stop', 'Pause', 'Reset', 'Suspend']:
                     wf.add_item(
-                        action, arg=action.lower() + ' ' + vm_name,
-                        uid=vm_name, valid=True, icon=vm_details.type, icontype='fileicon')
+                        action, arg=action.lower() + ' ' + vm_name, valid=True, icon=vm_details.type, icontype='fileicon')
 
             elif vm_details.state == "suspended":
                 for action in ['Resume']:
                     wf.add_item(
-                        action, arg=action.lower() + ' ' + vm_name,
-                        uid=vm_name, valid=True, icon=vm_details.type, icontype='fileicon')
+                        action, arg=action.lower() + ' ' + vm_name, valid=True, icon=vm_details.type, icontype='fileicon')
 
             elif vm_details.state == "running":
                 for action in ['Stop', 'Pause', 'Reset', 'Suspend', 'Restart']:
                     wf.add_item(
-                        action, arg=action.lower() + ' ' + vm_name,
-                        uid=vm_name, valid=True, icon=vm_details.type, icontype='fileicon')
+                        action, arg=action.lower() + ' ' + vm_name, valid=True, icon=vm_details.type, icontype='fileicon')
+                wf.add_item(
+                    "Kill", arg='stop ' + vm_name + ' --kill', valid=True, icon=vm_details.type, icontype='fileicon')
 
                 filename = "Screenshot\ " + \
                     strftime("%Y-%m-%d\ %H.%M.%S\ %p", gmtime()) + ".png"
                 wf.add_item(
-                    "Capture screenshot", "Saves a screenshot to Desktop", arg='capture ' + vm_name + ' --file ~/Desktop/' + filename,
-                    uid=vm_name, valid=True, icon=vm_details.type, icontype='fileicon')
+                    "Capture screenshot", "Saves a screenshot to Desktop", arg='capture ' + vm_name + ' --file ~/Desktop/' + filename, valid=True, icon=vm_details.type, icontype='fileicon')
 
             # Virtual Box
             elif vm_details.state in ["PoweredOff", "Saved"]:
                 wf.add_item(
-                    'Start', arg=vm_name + ' start',
-                    uid=vm_name, valid=True, icon=vm_details.type, icontype='fileicon')
+                    'Start', arg=vm_name + ' start', valid=True, icon=vm_details.type, icontype='fileicon')
 
             elif vm_details.state == "Paused":
                 for action in ['Resume', 'PowerOff', 'SaveState']:
                     wf.add_item(
-                        action, arg=vm_name + ' ' + action.lower(),
-                        uid=vm_name, valid=True, icon=vm_details.type, icontype='fileicon')
+                        action, arg=vm_name + ' ' + action.lower(), valid=True, icon=vm_details.type, icontype='fileicon')
 
             elif vm_details.state == "Running":
                 for action in ['PowerOff', 'Pause', 'Reset', 'SaveState', 'ACPIPowerButton', 'ACPISleepButton']:
                     wf.add_item(
-                        action, arg=vm_name + ' ' + action.lower(),
-                        uid=vm_name, valid=True, icon=vm_details.type, icontype='fileicon')
+                        action, arg=vm_name + ' ' + action.lower(), valid=True, icon=vm_details.type, icontype='fileicon')
 
                 filename = "Screenshot\ " + \
                     strftime("%Y-%m-%d\ %H.%M.%S\ %p", gmtime()) + ".png"
                 wf.add_item(
-                    "Capture screenshot", "Saves a screenshot to Desktop", arg=vm_name + ' screenshotpng ~/Desktop/' + filename,
-                    uid=vm_name, valid=True, icon=vm_details.type, icontype='fileicon')
+                    "Capture screenshot", "Saves a screenshot to Desktop", arg=vm_name + ' screenshotpng ~/Desktop/' + filename, valid=True, icon=vm_details.type, icontype='fileicon')
     else:
         for vm in vm_list:
             if vm.state.lower() == "running":
@@ -108,7 +100,7 @@ def get_vm_list():
     output = []
 
     if parallels_available:
-        for line in commands.getstatusoutput('prlctl list -a --no-header')[1].split('\n'):
+        for line in os.popen('prlctl list -a --no-header').readlines():
             vm_details = line.split()
             status = vm_details[1]
             vm_id = vm_details[0]
