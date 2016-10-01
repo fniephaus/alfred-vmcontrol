96f97222b04b9a3d22615e5df8687c41a71a9d0d
Steve Zhang <stevzhg@gmail.com>
2016-10-01 19:01:33 +0800

    fix #5 VBoxMange not found

--- src/vm_control.py
+++ src/vm_control.py
@@ -11,11 +11,11 @@ def execute(wf):
             # Virtual Box
             command = query.split('"')[2].split(' ')[1]
             if query[-5:] == "start":
-                os.popen('VBoxManage startvm %s' % query[:-6])
+                os.popen('/usr/local/bin/VBoxManage startvm %s' % query[:-6])
             elif command in ['poweroff', 'pause', 'reset', 'suspend', 'savestate', 'acpipowerbutton', 'acpisleepbutton', 'screenshotpng']:
                 if command in ['poweroff', 'reset', 'suspend']:
                     wf.clear_cache()
-                os.popen('VBoxManage controlvm %s' % query)
+                os.popen('/usr/local/bin/VBoxManage controlvm %s' % query)
         elif len(query.split()) > 1:
             # Parallels
             command = query.split()[0]
